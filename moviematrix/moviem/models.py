0818f015208f4682c3997efbd01bb80cb3acf2a9
User <user@SDAcourse.pl>
2023-11-04 14:40:41 +0200

    Models fix

--- moviematrix/moviem/models.py
+++ moviematrix/moviem/models.py
@@ -1,40 +1,76 @@
-
 from django.db import models
-
-# Create your models here.
 
 
 class Movie(models.Model):
-    title = models.CharField(max_length=200)
-    release_date = models.DateField()
-    director = models.ForeignKey('Director', on_delete=models.SET_NULL, null=True)
-    actors = models.ManyToManyField('Actor')
-    genres = models.ManyToManyField('Genre')
+    title = models.CharField(
+        max_length=200,
+        null=False,
+        blank=False,
+    )
+    release_date = models.DateField(
+        null=False,
+        blank=False,
+    )
+    director = models.ForeignKey(
+        'Director',
+        on_delete=models.PROTECT,
+        null=False,
+        blank=False,
+    )
+    actors = models.ManyToManyField(
+        'Actor',
+        null=False,
+        blank=False,
+    )
+    genres = models.ManyToManyField(
+        'Genre',
+        null=False,
+        blank=False,
+    )
 
     def __str__(self):
         return self.title
 
 
 class Director(models.Model):
-    name = models.CharField(max_length=200)
-    gender = models.CharField(max_length=15)
-    birthday = models.DateField()
+    name = models.CharField(max_length=200,
+                            null=False,
+                            blank=False,
+                            )
+    gender = models.CharField(max_length=15,
+                              null=False,
+                              blank=False,
+                              )
+    birthday = models.DateField(null=False,
+                                blank=False,
+                                )
 
     def __str__(self):
         return self.name
 
 
 class Actor(models.Model):
-    name = models.CharField(max_length=200)
-    gender = models.CharField(max_length=15)
-    birthday = models.DateField()
+    name = models.CharField(max_length=200,
+                            null=False,
+                            blank=False,
+                            )
+    gender = models.CharField(max_length=15,
+                              null=False,
+                              blank=False,
+                              )
+    birthday = models.DateField(null=False,
+                                blank=False,
+                                )
 
     def __str__(self):
         return self.name
 
 
 class Genre(models.Model):
-    name = models.CharField(max_length=150)
+    name = models.CharField(max_length=150,
+                            null=False,
+                            blank=False,
+                            )
 
     def __str__(self):
         return self.name
